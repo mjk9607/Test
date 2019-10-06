af6d06907db4785410d32718a222a7b38ae3bea3
mjk9607 <54826450+mjk9607@users.noreply.github.com>
2019-10-07 03:29:16 +0900

    zxc

--- Java/Test/src/Test.java
+++ Java/Test/src/Test.java
@@ -2,12 +2,16 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello Github!");
 		System.out.println("Hello Github Commit");
 		System.out.println("Hello Github branche");
 		System.out.println("Hello Github branche02");
 
+		System.out.println("Hello Github!");
+		System.out.println("Hello Github Commit");
+		System.out.println("Hello Github branche");
+		System.out.println("Hello Github branche02");
 	}
 
 }
