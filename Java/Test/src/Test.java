2dfd3b93b87e58b28297f519e22405ce4c929274
mjk9607 <54826450+mjk9607@users.noreply.github.com>
2019-10-07 03:15:24 +0900

    Test bran

--- Java/Test/src/Test.java
+++ Java/Test/src/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github!");
 		System.out.println("Hello Github Commit");
+		System.out.println("Hello Github branche");
 
 	}
 
