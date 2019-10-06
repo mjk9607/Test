6590f0171f72febde446aab20812b6287b495968
mjk9607 <54826450+mjk9607@users.noreply.github.com>
2019-10-07 03:34:51 +0900

    asd

--- Java/Test/src/Test.java
+++ Java/Test/src/Test.java
@@ -5,13 +5,14 @@ public class Test {
 		
 		System.out.println("Hello Github!");
 		System.out.println("Hello Github Commit");
-		System.out.println("Hello Github branche");
-		System.out.println("Hello Github branche02");
+		System.out.println("Hello Github branche07");
+		System.out.println("Hello Github branche08");
  
 		System.out.println("Hello Github!");
 		System.out.println("Hello Github Commit");
 		System.out.println("Hello Github branche");
 		System.out.println("Hello Github branche02");
+
 	}
 
 }
