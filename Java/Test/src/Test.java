6b731a593a219b56772295968f82b69bd007cce0
mjk9607 <54826450+mjk9607@users.noreply.github.com>
2019-10-27 15:29:33 +0900

    Test

--- Java/Test/src/Test.java
+++ Java/Test/src/Test.java
@@ -2,17 +2,9 @@
 public class Test {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello Github!");
-		System.out.println("Hello Github Commit");
-		System.out.println("Hello Github branche07");
-		System.out.println("Hello Github branche08");
 
- 
-		System.out.println("Hello Github!");
-		System.out.println("Hello Github Commit");
-		System.out.println("Hello Github branche");
-		System.out.println("Hello Github branche02");
+		System.out.println("Hello Github Master Commit");
+
 
 	}
 
