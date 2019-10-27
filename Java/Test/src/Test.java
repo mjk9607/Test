ad52a61871a0e5db17220511a0086c7444ec0834
mjk9607 <mjk9607@naver.com>
2019-10-27 17:38:32 +0900

    Test

--- Java/Test/src/Test.java
+++ Java/Test/src/Test.java
@@ -9,6 +9,7 @@ public class Test {
 		System.out.println("Hello master01");
 		System.out.println("Hello master02");
 		System.out.println("Hello fatch05 - 2");
+		System.out.println("Hello fatch05 - 3");
 
  
 	}
