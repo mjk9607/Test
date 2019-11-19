64eff0adcdcbb24a83c7b33a26b302df3c3282e0
mjk9607 <mjk9607@naver.com>
2019-11-20 01:16:01 +0900

    Test

--- Java/Test/src/Test.java
+++ Java/Test/src/Test.java
@@ -3,16 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello Github Master Commit");
-    
-		System.out.println("Hello master");
-		System.out.println("Hello master01");
-		System.out.println("Hello master02");
-		System.out.println("Hello fatch05 - 2");
-		System.out.println("I am Master");
+		System.out.println("First Commit");
 
-
- 
 	}
 
 }
